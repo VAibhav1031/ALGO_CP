b6260466da11da1e8bc2f00230c2356525d9c497
Vaibhav Tiwari <vaibhavgrand2004@gmail.com>
2025-05-21 02:51:47 +0530

    Updated strassen_multiplication.py

--- strassen_multiplication.py
+++ strassen_multiplication.py
@@ -1,9 +1,9 @@
 #Strassen  Multiplcation 
-
+import random
+import time
 # simple Multiplcation . 
 
 # lets say we 
-
 num = [[1,4],[9,6]]
 num_1 = [[2,3],[2,3]]
 
@@ -15,7 +15,7 @@ for i in range(len(num)):
             result[i][j] += (num[i][k]*num_1[k][j])
 
 # this is the simple Multiplcation you may see, why this  one is difficult one for many people 
-print(result)
+# print(result)
 
 
 # currently here we are using the nXn matrix so you must know  k  is the common part in both matrix size mxk kxp
@@ -79,10 +79,10 @@ def multi(A, B):
 
 
 
-    C11 = add(M1+M2)
-    C12 = add(M3+M4)
-    C21 = add(M5+M6)
-    C22 = add(M7+M8)
+    C11 = add(M1,M2)
+    C12 = add(M3,M4)
+    C21 = add(M5,M6)
+    C22 = add(M7,M8)
 
 
     return combine_quadrant(C11,C12,C21,C22)
@@ -120,8 +120,8 @@ def strassenMultiplication(A, B):
 
 
 
-        
-    
+def generate_matrix(n):
+    return [[random.randint(0,10) for _ in range(n)] for _ in range(n)]
 
 A1 = [[1, 2], [3, 4]]
 B1 = [[5, 6], [7, 8]]
@@ -135,5 +135,94 @@ A3 = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
 B3 = [[9, 8, 7, 6], [5, 4, 3, 2], [1, 1, 1, 1], [0, 0, 0, 0]]
 # Expected result: B itself (A is identity matrix)
 
-print(strassenMultiplication(A3,B3))
+
+def padUP(A):
+    r, c = len(A),len(A[0])
+    cur = max(r, c)
+    if cur & (cur - 1) == 0:
+        # just  add the number of required in the less one
+        if min(r, c) == c:
+            return [row + [0]*(r - c)  for row in A]
+        else:
+            return A + [[0 for _ in range(cur)]for _ in range(cur - r)]
+
+    req = 1
+    while req < cur:
+        req <<= 1 
+    
+    return [row + [0]*(req-c)  for row in A] + [[0 for _ in range(req)]for _ in range(req-r)]
+   
+
+def strassen_pad_up(A, B):
+    r1, c1 = len(A), len(A[0])
+    r2, c2 = len(B), len(B[0])
+
+    if (r1, c1) >= (r2, c2):
+        new_A = padUP(A)
+        req_r, req_c = len(new_A) - r2, len(new_A[0]) - c2
+        new_B = [row + [0] * req_c for row in B] + [[0] * (c2 + req_c) for _ in range(req_r)]
+    else:
+        new_B = padUP(B)
+        req_r, req_c = len(new_B) - r1, len(new_B[0]) - c1
+        new_A = [row + [0] * req_c for row in A] + [[0] * (c1 + req_c) for _ in range(req_r)]
+
+    return new_A, new_B
+
+    
+# N = 256 
+# new_A = generate_matrix(N)
+# new_B = generate_matrix(N)
+#
+# # print(new_A)
+# st = time.time()
+# multi(new_A,new_B)
+# et = time.time()
+# print(f"Divide and conquer(fcked) time: {(et-st)*1000:.2f}ms")
+#
+# st_ = time.time()
+# strassenMultiplication(new_A,new_B)
+# et_ = time.time()
+# print(f"Strassen time :{(et_-st_)*1000:.2f}ms")
+#
+A = [[1, 2], [3, 4]]
+B = [[5, 6], [7, 8], [9, 10]]
+
+new_A, new_B = strassen_pad_up(A, B)
+print("A:")
+for row in new_A:
+    print(row)
+print("B:")
+for row in new_B:
+    print(row)
+
+
+def unpad(matrix, r, c):
+    return [row[:c] for row in matrix[:r]]
+
+def test_strassen():
+    A = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    B = [
+        [9, 8, 7],
+        [6, 5, 4],
+        [3, 2, 1]
+    ]
+    print()
+    padded_A, padded_B = strassen_pad_up(A, B)
+    for row in padded_A:
+        print(row)
+    print()
+    for row in padded_B:
+        print(row)
+    padded_result = strassenMultiplication(padded_A, padded_B)
+    final_result = unpad(padded_result, len(A), len(B[0]))
+    
+    print("\nFinal Result:")
+    for row in final_result:
+        print(row)
+
+test_strassen()
 
